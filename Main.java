cbd8ded65bc5d75b60875480cd870fcf02bc71e7
James Hill <JamesTristanHill1998@gmail.com>
2026-07-01 17:20:25 -0500

    Asks the user for their age and prints to the terminal

--- Main.java
+++ Main.java
@@ -4,15 +4,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
             ///Creates a new scanner to ask for the name.
-        Scanner input = new Scanner.(System.in);
+        Scanner input = new Scanner(System.in);
             ///Sends console log to prompt user for their name.
-        System.out.print("What's you name: ");
+        System.out.print("What's your name: ");
             ///Collects the users name
         String name = input.nextLine();
             ///Prints the users name to the console.
         System.out.printf("Welcome to GitHub, %s!", name);
             ///Closes the scanner.
+        System.out.println();
+        System.out.print("How old are you: ");
+
+        int age = input.nextInt();
+        System.out.println();
+        System.out.printf("%s, you're %d years old!", name, age);
         input.close();
+
 
     }
 
