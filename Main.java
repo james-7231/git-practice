73e71a09d553b0cbc2772602aca0fab91dcbe571
James Hill <JamesTristanHill1998@gmail.com>
2026-07-01 17:07:30 -0500

    Add first Java program

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args){
+            ///Creates a new scanner to ask for the name.
+        Scanner input = new Scanner.(System.in);
+            ///Sends console log to prompt user for their name.
+        System.out.print("What's you name: ");
+            ///Collects the users name
+        String name = input.nextLine();
+            ///Prints the users name to the console.
+        System.out.printf("Thank you, %s!", name);
+            ///Closes the scanner.
+        input.close();
+
+    }
+
+
+
+}
